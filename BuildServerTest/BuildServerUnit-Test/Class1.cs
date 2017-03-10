0d2b946743be35678104a990b82d27de86e4715f
Ahmed Fahed <Fahed@soft-park.de>
2017-03-10 11:49:33 +0100

    use Unit Test

--- BuildServerTest/BuildServerUnit-Test/Class1.cs
+++ BuildServerTest/BuildServerUnit-Test/Class1.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using NUnit.Framework;
 
 namespace BuildServerUnit_Test
 {
-
+    [TestFixture]
     public class Class1
     {
-      
+      [Test]
         public void test1()
         {
             
